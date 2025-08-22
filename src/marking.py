a0b6f38b8abe9ddfc2a4513c95bd8a02da4414ba
benmandrew <benmandrew@gmail.com>
2025-08-22 12:32:24 +0100

    Extracted out ugly marking code

--- src/marking.py
+++ src/marking.py
@@ -212,6 +212,87 @@ class Marking:
     def get(self, f: m.Mtl, i: int) -> bool:
         return self[f][self.trace.idx(i)]
 
+    def _get_and(self, left: m.Mtl, right: m.Mtl) -> list[bool]:
+        return [l and r for l, r in zip(self[left], self[right])]  # noqa: E741
+
+    def _get_or(self, left: m.Mtl, right: m.Mtl) -> list[bool]:
+        return [l or r for l, r in zip(self[left], self[right])]  # noqa: E741
+
+    def _get_implies(self, left: m.Mtl, right: m.Mtl) -> list[bool]:
+        return [
+            (not l) or r for l, r in zip(self[left], self[right])  # noqa: E741
+        ]
+
+    def _get_eventually(
+        self,
+        operand: m.Mtl,
+        interval: m.Interval,
+    ) -> list[bool]:
+        vs = self[operand]
+        bs = [False] * len(vs)
+        for i in range(len(bs)):
+            right_idx = interval[1] + 1 if interval[1] is not None else len(vs)
+            bs[i] = any(
+                vs[self.trace.idx(j)]
+                for j in range(i + interval[0], i + right_idx)
+            )
+        return bs
+
+    def _get_always(self, operand: m.Mtl, interval: m.Interval) -> list[bool]:
+        vs = self[operand]
+        bs = [False] * len(vs)
+        for i in range(len(bs)):
+            right_idx = interval[1] + 1 if interval[1] is not None else len(vs)
+            bs[i] = all(
+                vs[self.trace.idx(j)]
+                for j in range(i + interval[0], i + right_idx)
+            )
+        return bs
+
+    def _get_until(
+        self,
+        left: m.Mtl,
+        right: m.Mtl,
+        interval: m.Interval,
+    ) -> list[bool]:
+        rights = self[right]
+        lefts = self[left]
+        bs = [False] * len(rights)
+        for i in range(len(bs)):
+            right_idx = (
+                interval[1] + 1 if interval[1] is not None else len(rights) - i
+            )
+            for j in range(i + interval[0], i + right_idx):
+                k = self.trace.idx(j)
+                if rights[k]:
+                    bs[i] = True
+                    break
+                if not lefts[k]:
+                    break
+        return bs
+
+    def _get_release(
+        self,
+        left: m.Mtl,
+        right: m.Mtl,
+        interval: m.Interval,
+    ) -> list[bool]:
+        rights = self[right]
+        lefts = self[left]
+        bs = [False] * len(rights)
+        for i in range(len(bs)):
+            right_idx = (
+                interval[1] + 1 if interval[1] is not None else len(rights) - i
+            )
+            for j in range(i + interval[0], i + right_idx):
+                k = self.trace.idx(j)
+                if not rights[k]:
+                    break
+                if lefts[k]:
+                    bs[i] = True
+                    break
+        return bs
+
     def __getitem__(self, f: m.Mtl) -> list[bool]:
         if f in self.markings:
             return self.markings[f]
@@ -221,78 +302,19 @@ class Marking:
         if isinstance(f, m.Not):
             bs = [not v for v in self[f.operand]]
         elif isinstance(f, m.And):
-            bs = [
-                left and right
-                for left, right in zip(self[f.left], self[f.right])
-            ]
+            bs = self._get_and(f.left, f.right)
         elif isinstance(f, m.Or):
-            bs = [
-                left or right
-                for left, right in zip(self[f.left], self[f.right])
-            ]
+            bs = self._get_or(f.left, f.right)
         elif isinstance(f, m.Implies):
-            bs = [
-                (not left) or right
-                for left, right in zip(self[f.left], self[f.right])
-            ]
+            bs = self._get_implies(f.left, f.right)
         elif isinstance(f, m.Eventually):
-            vs = self[f.operand]
-            bs = [False] * len(vs)
-            for i in range(len(bs)):
-                right_idx = (
-                    f.interval[1] + 1 if f.interval[1] is not None else len(vs)
-                )
-                bs[i] = any(
-                    vs[self.trace.idx(j)]
-                    for j in range(i + f.interval[0], i + right_idx)
-                )
+            bs = self._get_eventually(f.operand, f.interval)
         elif isinstance(f, m.Always):
-            vs = self[f.operand]
-            bs = [False] * len(vs)
-            for i in range(len(bs)):
-                right_idx = (
-                    f.interval[1] + 1 if f.interval[1] is not None else len(vs)
-                )
-                bs[i] = all(
-                    vs[self.trace.idx(j)]
-                    for j in range(i + f.interval[0], i + right_idx)
-                )
-            self.markings[f] = bs
-            return bs
+            bs = self._get_always(f.operand, f.interval)
         elif isinstance(f, m.Until):
-            rights = self[f.right]
-            lefts = self[f.left]
-            bs = [False] * len(rights)
-            for i in range(len(bs)):
-                right_idx = (
-                    f.interval[1] + 1
-                    if f.interval[1] is not None
-                    else len(rights) - i
-                )
-                for j in range(i + f.interval[0], i + right_idx):
-                    k = self.trace.idx(j)
-                    if rights[k]:
-                        bs[i] = True
-                        break
-                    if not lefts[k]:
-                        break
+            bs = self._get_until(f.left, f.right, f.interval)
         elif isinstance(f, m.Release):
-            rights = self[f.right]
-            lefts = self[f.left]
-            bs = [False] * len(rights)
-            for i in range(len(bs)):
-                right_idx = (
-                    f.interval[1] + 1
-                    if f.interval[1] is not None
-                    else len(rights) - i
-                )
-                for j in range(i + f.interval[0], i + right_idx):
-                    k = self.trace.idx(j)
-                    if not rights[k]:
-                        break
-                    if lefts[k]:
-                        bs[i] = True
-                        break
+            bs = self._get_release(f.left, f.right, f.interval)
         else:
             msg = f"Unsupported MTL construct: {f}"
             raise TypeError(msg)
