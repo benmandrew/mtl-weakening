1f2bc25a54ca8642a62551950c926c48f54ce221
benmandrew <benmandrew@gmail.com>
2025-08-27 15:38:42 +0100

    Fix ruff warnings

--- plot.py
+++ plot.py
@@ -1,19 +1,23 @@
 import csv
+import pathlib
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 rows = []
-with open("weakenings.csv", "r", encoding="utf-8") as f:
-    f = csv.reader(f)
-    for row in f:
-        rows.append([0 if x.isspace() or x == "" else int(x) for x in row])
+with pathlib.Path("weakenings.csv").open(encoding="utf-8") as f:
+    csv_reader = csv.reader(f)
+    rows = [
+        [0 if x.isspace() or x == "" else int(x) for x in row]
+        for row in csv_reader
+    ]
     cex_len, left_interval, right_interval = zip(*rows)
 
 fig, ax = plt.subplots()
 ax.grid()
 
-ax.set_xticks(np.arange(0, len(cex_len)+1, 20))
-ax.set_yticks(range(0, max(right_interval)+1))
+ax.set_xticks(np.arange(0, len(cex_len) + 1, 20))
+ax.set_yticks(range(max(right_interval) + 1))
 ax.plot(right_interval, label="Right Interval")
 ax.set_xlabel("Run")
 ax.set_ylabel("Value")
