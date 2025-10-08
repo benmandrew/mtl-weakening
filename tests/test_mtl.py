9ff42f31d1b6a7cacbd644072ea2179a3df721a5
benmandrew <benmandrew@gmail.com>
2025-10-08 16:08:51 +0100

    Set terminal dimensions for spawned expect tests only, to not mess up the calling terminal

--- tests/test_mtl.py
+++ tests/test_mtl.py
@@ -6,43 +6,43 @@ from src.logic import ltl, mtl, parser
 class TestMtlToLtl(unittest.TestCase):
     def test_eventually_upper_bound(self) -> None:
         formula = parser.parse_mtl("F[2,4] p")
-        expected = parser.parse_ltl("X (X (p | X (p | X (p))))")
+        expected = parser.parse_nuxmv_ltl("X (X (p | X (p | X (p))))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_eventually_infinite_upper_bound(self) -> None:
         formula = parser.parse_mtl("F[1,∞] p")
-        expected = parser.parse_ltl("X (F p)")
+        expected = parser.parse_nuxmv_ltl("X (F p)")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_always_upper_bound(self) -> None:
         formula = parser.parse_mtl("G[1,3] p")
-        expected = parser.parse_ltl("X (p & X (p & X (p)))")
+        expected = parser.parse_nuxmv_ltl("X (p & X (p & X (p)))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_always_infinite_upper_bound(self) -> None:
         formula = parser.parse_mtl("G[3,∞] p")
-        expected = parser.parse_ltl("X (X (X (G p)))")
+        expected = parser.parse_nuxmv_ltl("X (X (X (G p)))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_until_upper_bound(self) -> None:
         formula = parser.parse_mtl("p U[1,2] q")
-        expected = parser.parse_ltl("X (q | (p & X (q)))")
+        expected = parser.parse_nuxmv_ltl("X (q | (p & X (q)))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_until_infinite_upper_bound(self) -> None:
         formula = parser.parse_mtl("p U[2,∞] q")
-        expected = parser.parse_ltl("X (X (p U q))")
+        expected = parser.parse_nuxmv_ltl("X (X (p U q))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_release_upper_bound(self) -> None:
         formula = parser.parse_mtl("p R[1,2] q")
-        expected = parser.parse_ltl(
+        expected = parser.parse_nuxmv_ltl(
             "X ((q & X (q)) | (p & q) | (q & X (p & q)))",
         )
         result = mtl.mtl_to_ltl(formula)
@@ -50,17 +50,17 @@ class TestMtlToLtl(unittest.TestCase):
 
     def test_release_infinite_upper_bound(self) -> None:
         formula = parser.parse_mtl("p R[2,∞] q")
-        expected = parser.parse_ltl("X (X (p R q))")
+        expected = parser.parse_nuxmv_ltl("X (X (p R q))")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
         formula = parser.parse_mtl("p R q")
-        expected = parser.parse_ltl("(p R q)")
+        expected = parser.parse_nuxmv_ltl("(p R q)")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
     def test_boolean(self) -> None:
         formula = parser.parse_mtl("TRUE & FALSE")
-        expected = parser.parse_ltl("TRUE & FALSE")
+        expected = parser.parse_nuxmv_ltl("TRUE & FALSE")
         result = mtl.mtl_to_ltl(formula)
         self.assertEqual(ltl.to_nuxmv(result), ltl.to_nuxmv(expected))
 
