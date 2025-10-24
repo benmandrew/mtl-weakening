69c3bd510b85e48fcf3a979025858ad6edd536fe
benmandrew <benmandrew@gmail.com>
2025-10-24 15:11:29 +0100

    Make Spin trace analysis applicable to other models

--- src/trace_analysis/spin_trace.py
+++ src/trace_analysis/spin_trace.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import typing
+from collections import OrderedDict
 
 from src import marking
 
@@ -10,46 +11,44 @@ def parse_variables(s: str) -> dict[str, int | str]:
     return typing.cast("dict[str, int | str]", json.loads(s))
 
 
-def expand_state(state: dict[str, int | str]) -> dict[str, int]:
+def expand_state(
+    state: dict[str, int | str],
+    state_values: OrderedDict[str, None],
+) -> dict[str, int]:
     expanded: dict[str, int] = {}
     for k, v in state.items():
         if isinstance(v, int):
             expanded[k] = v
         elif isinstance(v, str):
-            for enum_value in [
-                "resting",
-                "leavingHome",
-                "randomWalk",
-                "moveToFood",
-                "scanArena",
-                "grabFood",
-                "moveToHome",
-                "deposit",
-                "homing",
-            ]:
-                expanded[f"{enum_value}_p"] = v == enum_value
+            for val in state_values:
+                expanded[f"{val}_p"] = v == val
         else:
             msg = f"Unexpected variable type: {type(v)}"
             raise TypeError(msg)
     return expanded
 
 
-def clear_nonappearing_states(states: list[dict[str, int]]) -> None:
-    for enum_value in [
-        "resting_p",
-        "leavingHome_p",
-        "randomWalk_p",
-        "moveToFood_p",
-        "scanArena_p",
-        "grabFood_p",
-        "moveToHome_p",
-        "deposit_p",
-        "homing_p",
-    ]:
-        appears = any(state.get(enum_value, 0) == 1 for state in states)
+def clear_nonappearing_states(
+    states: list[dict[str, int]],
+    state_values: OrderedDict[str, None],
+) -> None:
+    state_variables = {f"{val}_p" for val in state_values}
+    for var in state_variables:
+        appears = any(state.get(var, 0) == 1 for state in states)
         if not appears:
             for state in states:
-                state.pop(enum_value)
+                state.pop(var)
+
+
+def get_state_values(
+    states: list[dict[str, int | str]],
+) -> OrderedDict[str, None]:
+    state_values: OrderedDict[str, None] = OrderedDict()
+    for state in states:
+        for k, v in state.items():
+            if isinstance(v, str) and k == "state":
+                state_values[v] = None
+    return state_values
 
 
 def parse(s: str) -> marking.Trace:
@@ -64,8 +63,9 @@ def parse(s: str) -> marking.Trace:
         else:
             states.append(parse_variables(line))
             state_i += 1
-    expanded_states = [expand_state(state) for state in states]
-    clear_nonappearing_states(expanded_states)
+    state_values = get_state_values(states)
+    expanded_states = [expand_state(state, state_values) for state in states]
+    clear_nonappearing_states(expanded_states, state_values)
     return marking.Trace(
         typing.cast("list[dict[str, bool | int | str]]", expanded_states),
         loop_i,
